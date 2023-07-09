848cb6196a13ebfd1cc8022425faa1632144f5d3
joeyissa <joeyissa59@gmail.com>
2023-07-09 11:52:03 -0400

    working on site navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+}
