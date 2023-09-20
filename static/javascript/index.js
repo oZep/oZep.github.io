2b45a59fff45d3922d8659cac1014d96f956c64d
joeyissa <joeyissa59@gmail.com>
2023-09-20 00:46:36 -0400

    updated to make index file visible

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -4,11 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
     function type() {
         letters.forEach((letter, index) => {
             setTimeout(() => {
-                letter.textContent = 'Joey Issa'[index];
+                if (index === 4) {
+                    letter.innerHTML = '&nbsp;';
+                } else {
+                    letter.textContent = 'Joey Issa.'[index];
+                }
                 letter.style.opacity = '1';
             }, index * 100); // Adjust timing based on typing speed
         });
     }
 
     type();
+
+    function resetAnimation() {
+        letters.forEach(letter => {
+            letter.textContent = '';
+            letter.style.opacity = '0';
+        });
+        setTimeout(type, 1000); // Start typing after 1 second
+    }
+
+    setInterval(resetAnimation, 60000); // Repeat every 90 seconds (1.30 minutes)
 });
